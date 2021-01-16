deb94f657422227b702f42f843ac0b61c4d649b7
Lucas Diaz <lucasdiaz22b@gmail.com>
2021-01-16 09:36:48 -0500

    changed the way the answer btn is layed out and begun to work on js to connect questions

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
+
+var btn = document.getElementsByClassName("btn");
 startButton = document.getElementById("start-btn");
 questionContainer = document.getElementById("question-container");
+question = document.getElementById("question");
 
 function startQuiz(){
     questionContainer.classList.remove("hide");
     startButton.classList.add("hide");
+
+    for (let i = 0; i < questionList.length; i++) {
+     question.append(questionList[i]);
+    }
 }
 
 
 startButton.addEventListener("click", startQuiz);
+
+
+
+questionList = [
+    "Question 1",
+    "Question 2",
+    "Question 3",
+    "Question 4",
+    "Question 5",
+]
+
+
+
+
+
 
 
